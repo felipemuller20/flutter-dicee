c86a7ae3af8b12d47587b21f13d697eddd9aea4a
felipe <felipeavmuller@gmail.com>
2023-10-22 21:40:31 -0300

    start project

---
+++ android/app/src/main/kotlin/com/gmailfelipeavmuller/dicee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gmailfelipeavmuller.dicee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
